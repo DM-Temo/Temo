a1573fc3caa43372bbf7a9540e7164ceb7ba9b82
hyunj <ahddkfv@gmail.com>
2024-09-13 19:01:32 +0900

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Import the BoM for the Firebase platform
+    implementation ("com.google.firebase:firebase-bom:32.1.0")
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation ("com.google.firebase:firebase-auth-ktx")
+
+    // Also add the dependency for the Google Play services library and specify its version
+    implementation ("com.google.android.gms:play-services-auth:20.5.0")
 }
--- app/src/main/java/com/example/temo/MainActivity.kt
+++ app/src/main/java/com/example/temo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,10 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             TemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LoginScreen(innerPadding)
                 }
             }
         }
@@ -31,17 +29,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LoginScreen(innerPadding: PaddingValues) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TemoTheme {
-        Greeting("Android")
     }
 }
